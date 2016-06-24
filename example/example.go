d809dc5c82925a81f6d1a2c91fb912f946cb8486
Ivan Pushkin <pushkin13@bk.ru>
2016-06-24 14:57:10 +0300

    fix example

--- example/example.go
+++ example/example.go
@@ -26,33 +26,6 @@ func main() {
 }
 
 //
-// 1. Verifier
-//
-
-// Verifier verifies connection
-type Verifier struct{}
-
-// Implementing pwshandler.RequestVerifier interface
-func (v *Verifier) Verify(ws *websocket.Conn) error {
-	// Verify user and password
-	user, password, ok := ws.Request().BasicAuth()
-	if !ok {
-		return fmt.Errorf("access forbidden")
-	}
-	if err := v.VerifyUserPassword(user, password); err != nil {
-		return fmt.Errorf("access forbidden: %s", err)
-	}
-
-	return nil
-}
-
-// VerifyUserPassword returns error if user or password is invalid
-func (v *Verifier) VerifyUserPassword(user, password string) error {
-	// Very secure verifying of user and password
-	return nil
-}
-
-//
 // 2. PoolManager
 //
 
